c75add0d193e3aa939002c4a30c5f17d768ac209
ShuhanLi <shuhan.li@flexiv.com>
2024-01-18 13:40:33 -0800

    Added PER module

--- training_data_hindsight_augmentation_with_transformer_world_models/agent_train.py
+++ training_data_hindsight_augmentation_with_transformer_world_models/agent_train.py
@@ -125,3 +125,15 @@ class AgentTrainer():
       self.critic.load_state_dict(checkpoint['critic_state_dict'])
       self.target_critic.load_state_dict(checkpoint['target_critic_state_dict'])
       self.critic_optimizer.load_state_dict(checkpoint['critic_optimizer_state_dict'])
+    
+    def save_checkpoint(self, episode):
+      checkpoint = {'episode': episode,
+      'actor_state_dict': self.agent_trainer.actor.state_dict(),
+      'target_actor_state_dict': self.agent_trainer.target_actor.state_dict(),
+      'actor_optimizer_state_dict': self.agent_trainer.actor_optimizer.state_dict(),
+      'critic_state_dict': self.agent_trainer.critic.state_dict(),
+      'target_critic_state_dict': self.agent_trainer.target_critic.state_dict(),
+      'critic_optimizer_state_dict': self.agent_trainer.critic_optimizer.state_dict()}
+
+      torch.save(checkpoint, '/content/drive/My Drive/check_point')
+      print('Model Saved')
