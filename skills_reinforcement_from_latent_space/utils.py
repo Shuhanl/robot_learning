349084b61cea0b04742bfc1c309ba67645fbce71
ShuhanLi <shuhan.li@flexiv.com>
2024-02-14 03:00:33 +0800

    Added mamba module

--- skills_reinforcement_from_latent_space/utils.py
+++ skills_reinforcement_from_latent_space/utils.py
@@ -1,14 +1,6 @@
 import torch
 import torch.nn.functional as F
 
-def image_process(image):
-    image = torch.tensor(image.transpose(2, 0, 1), dtype=torch.float32).unsqueeze(0)
-    image = 0.2989 * image[:, 0, :, :] + 0.5870 * image[:, 1, :, :] + 0.1140 * image[:, 2, :, :]
-    image = image.unsqueeze(1)
-    image = F.interpolate(image, size=(64, 64), mode='bilinear', align_corners=False)
-    return image
-
-
 def save_checkpoint(self, episode):
     checkpoint = {'episode': episode,
     'actor_state_dict': self.agent_trainer.actor.state_dict(),
--- training_data_hindsight_augmentation_with_world_models/mamba.py
+++ training_data_hindsight_augmentation_with_world_models/mamba.py
@@ -1,17 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
 from torch.nn import functional as F
 from einops import rearrange
 from tqdm import tqdm
-
-import math
-import os
 import urllib.request
 from zipfile import ZipFile
 
-from transformers import AutoTokenizer
 import parameters as params
 
 torch.autograd.set_detect_anomaly(True)
@@ -29,21 +24,28 @@ class S6(nn.Module):
         self.d_model = d_model
         self.state_size = state_size
 
-        #self.A = nn.Parameter(torch.ones(d_model, state_size, device=device))
-        self.A = nn.Parameter(F.normalize(torch.ones(d_model, state_size, device=device), p=2, dim=-1))
+        # self.A = nn.Parameter(torch.ones(d_model, state_size, device=device))
+        self.A = nn.Parameter(F.normalize(torch.ones(
+            d_model, state_size, device=device), p=2, dim=-1))
         nn.init.xavier_uniform_(self.A)
 
-        self.B = torch.zeros(params.batch_size, self.seq_len, self.state_size, device=device)
-        self.C = torch.zeros(params.batch_size, self.seq_len, self.state_size, device=device)
+        self.B = torch.zeros(params.batch_size, self.seq_len,
+                             self.state_size, device=device)
+        self.C = torch.zeros(params.batch_size, self.seq_len,
+                             self.state_size, device=device)
 
-        self.delta = torch.zeros(params.batch_size, self.seq_len, self.d_model, device=device)
-        self.dA = torch.zeros(params.batch_size, self.seq_len, self.d_model, self.state_size, device=device)
-        self.dB = torch.zeros(params.batch_size, self.seq_len, self.d_model, self.state_size, device=device)
+        self.delta = torch.zeros(
+            params.batch_size, self.seq_len, self.d_model, device=device)
+        self.dA = torch.zeros(params.batch_size, self.seq_len,
+                              self.d_model, self.state_size, device=device)
+        self.dB = torch.zeros(params.batch_size, self.seq_len,
+                              self.d_model, self.state_size, device=device)
 
         # h should have dimensions [batch_size, seq_len, d_model, state_size]
-        self.h = torch.zeros(params.batch_size, self.seq_len, self.d_model, self.state_size, device=device)
-        self.y = torch.zeros(params.batch_size, self.seq_len, self.d_model, device=device)
-
+        self.h = torch.zeros(params.batch_size, self.seq_len,
+                             self.d_model, self.state_size, device=device)
+        self.y = torch.zeros(params.batch_size, self.seq_len,
+                             self.d_model, device=device)
 
     def discretization(self):
         # discretization function is defined based on the MAMBA paper's description using ZOH on page 28
@@ -70,14 +72,14 @@ class S6(nn.Module):
         """
 
         # inverse() only supports square matrix
-        #dB = torch.matmul(torch.inverse(A * delta), torch.matmul(dA - torch.eye(A.shape[0]), B))
+        # dB = torch.matmul(torch.inverse(A * delta), torch.matmul(dA - torch.eye(A.shape[0]), B))
         self.dB = torch.einsum("bld,bln->bldn", self.delta, self.B)
 
         # https://github.com/state-spaces/mamba/blob/0131c1e94a46fc9f70bcfc9d57962963bb2f0b9e/mamba_ssm/modules/mamba_simple.py#L240
-        #dA = torch.matrix_exp(A * delta)  # matrix_exp() only supports square matrix
+        # dA = torch.matrix_exp(A * delta)  # matrix_exp() only supports square matrix
         self.dA = torch.exp(torch.einsum("bld,dn->bldn", self.delta, self.A))
-        #print(f"self.dA.shape = {self.dA.shape}")
-        #print(f"self.dA.requires_grad = {self.dA.requires_grad}")
+        # print(f"self.dA.shape = {self.dA.shape}")
+        # print(f"self.dA.requires_grad = {self.dA.requires_grad}")
 
         return self.dA, self.dB
 
@@ -90,60 +92,37 @@ class S6(nn.Module):
         # Uses ZOH as in MAMBA, Hungry Hippo still uses bilinear transform for discretization
         self.discretization()
 
-        if DIFFERENT_H_STATES_RECURRENT_UPDATE_MECHANISM:  # this will trigger in-place runtime error if without using `h_new`
-          
-            global current_batch_size
-            current_batch_size = x.shape[0]
+        # h should have dimensions [batch_size, seq_len, d_model, state_size]
+        h = torch.zeros(x.size(0), self.seq_len, self.d_model,
+                        self.state_size, device=x.device)
+        y = torch.zeros_like(x)
 
-            if self.h.shape[0] != current_batch_size:
-                #print("Adjusting h_new for the different batch size of input data `x`")
-                different_batch_size = True
+        h = torch.einsum('bldn,bldn->bldn', self.dA, h) + \
+            rearrange(x, "b l d -> b l d 1") * self.dB
 
-                # Resize self.h to match the current batch size
-                h_new =  torch.einsum('bldn,bldn->bldn', self.dA, self.h[:current_batch_size, ...]) + rearrange(x, "b l d -> b l d 1") * self.dB
+        # y needs to have a shape of [batch_size, seq_len, d_model]
+        y = torch.einsum('bln,bldn->bld', self.C, h)
 
-            else:
-                different_batch_size = False
-                h_new =  torch.einsum('bldn,bldn->bldn', self.dA, self.h) + rearrange(x, "b l d -> b l d 1") * self.dB
+        return y
 
-            # y needs to have a shape of [batch_size, seq_len, d_model]
-            self.y = torch.einsum('bln,bldn->bld', self.C, h_new)
 
-            # Update self.h with the detached state of h_new
-            # Only do this if retaining gradients for self.h is not necessary for backprop
-            # Otherwise, store h_new in a temporary list and update self.h after the loop
-            global temp_buffer
-            temp_buffer = h_new.detach().clone() if not self.h.requires_grad else h_new.clone()
-  
-            return self.y
-
-        else:  # this will not trigger in-place runtime error
-            # h should have dimensions [batch_size, seq_len, d_model, state_size]
-            h = torch.zeros(x.size(0), self.seq_len, self.d_model, self.state_size, device=x.device)
-            y = torch.zeros_like(x)
-
-            h =  torch.einsum('bldn,bldn->bldn', self.dA, h) + rearrange(x, "b l d -> b l d 1") * self.dB
-
-            # y needs to have a shape of [batch_size, seq_len, d_model]
-            y = torch.einsum('bln,bldn->bld', self.C, h)
-
-            return y
-        
 class RMSNorm(nn.Module):
     def __init__(self,
                  d_model: int,
                  eps: float = 1e-5,
-                 device: str ='cuda'):
+                 device: str = 'cuda'):
         super().__init__()
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(d_model, device=device))
 
-
     def forward(self, x):
-        output = x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps) * self.weight
+        output = x * \
+            torch.rsqrt(x.pow(2).mean(-1, keepdim=True) +
+                        self.eps) * self.weight
 
         return output
-    
+
+
 class MambaBlock(nn.Module):
     def __init__(self, seq_len, d_model, state_size, device):
         super(MambaBlock, self).__init__()
@@ -163,7 +142,8 @@ class MambaBlock(nn.Module):
         self.S6 = S6(seq_len, 2*d_model, state_size, device)
 
         # Add 1D convolution with kernel size 3
-        self.conv = nn.Conv1d(seq_len, seq_len, kernel_size=3, padding=1, device=device)
+        self.conv = nn.Conv1d(
+            seq_len, seq_len, kernel_size=3, padding=1, device=device)
 
         # Add linear layer for conv output
         self.conv_linear = nn.Linear(2*d_model, 2*d_model, device=device)
@@ -182,37 +162,39 @@ class MambaBlock(nn.Module):
         x = self.norm(x)
 
         x_proj = self.inp_proj(x)
-        #print(f"x_proj.shape = {x_proj.shape}")
+        # print(f"x_proj.shape = {x_proj.shape}")
 
         # Add 1D convolution with kernel size 3
         x_conv = self.conv(x_proj)
-        #print(f"x_conv.shape = {x_conv.shape}")
+        # print(f"x_conv.shape = {x_conv.shape}")
 
         x_conv_act = F.silu(x_conv)
-        #print(f"x_conv_act.shape = {x_conv_act.shape}")
+        # print(f"x_conv_act.shape = {x_conv_act.shape}")
 
         # Add linear layer for conv output
         x_conv_out = self.conv_linear(x_conv_act)
-        #print(f"x_conv_out.shape = {x_conv_out.shape}")
+        # print(f"x_conv_out.shape = {x_conv_out.shape}")
 
         x_ssm = self.S6(x_conv_out)
-        x_act = F.silu(x_ssm)  # Swish activation can be implemented as x * sigmoid(x)
-        #print(f"x_act.shape = {x_act.shape}")
+        # Swish activation can be implemented as x * sigmoid(x)
+        x_act = F.silu(x_ssm)
+        # print(f"x_act.shape = {x_act.shape}")
 
         # residual skip connection with nonlinearity introduced by multiplication
         x_residual = F.silu(self.D(x))
-        #print(f"x_residual.shape = {x_residual.shape}")
+        # print(f"x_residual.shape = {x_residual.shape}")
         x_combined = x_act * x_residual
-        #print(f"x_combined.shape = {x_combined.shape}")
+        # print(f"x_combined.shape = {x_combined.shape}")
 
         x_out = self.out_proj(x_combined)
-        #print(f"x_out.shape = {x_out.shape}")
+        # print(f"x_out.shape = {x_out.shape}")
 
         return x_out
 
-class Mamba(nn.Module):
+
+class MambaModel(nn.Module):
     def __init__(self, seq_len, d_model, state_size, device):
-        super(Mamba, self).__init__()
+        super(MambaModel, self).__init__()
         self.mamba_block1 = MambaBlock(seq_len, d_model, state_size, device)
         self.mamba_block2 = MambaBlock(seq_len, d_model, state_size, device)
         self.mamba_block3 = MambaBlock(seq_len, d_model, state_size, device)
@@ -222,4 +204,3 @@ class Mamba(nn.Module):
         x = self.mamba_block2(x)
         x = self.mamba_block3(x)
         return x
-
