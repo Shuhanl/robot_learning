56606d4a5199bdc328181032494c40dbeedf1549
ShuhanLi <shuhan.li@flexiv.com>
2024-09-14 20:01:38 -0700

    temp save

--- tactile_vision_data/robot/flexiv.py
+++ tactile_vision_data/robot/flexiv.py
@@ -16,7 +16,7 @@ class FlexivRobot:
         Initialize.
 
         Args:
-            gripper_robot_sn: ["192.168.3.100"], robot_ip
+            gripper_robot_sn
 
         Raises:
             RuntimeError: error occurred when ip_address is None.
@@ -29,114 +29,11 @@ class FlexivRobot:
         self.robot = None
 
         self.init_robot()
-        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
         # self.init_gripper()
 
         self.joint_limits_low = np.array([-2.7925, -2.2689, -2.9671, -1.8675, -2.9671, -1.3963, -2.9671]) + 0.1
         self.joint_limits_high = np.array([2.7925, 2.2689, 2.9671, 2.6878, 2.9671, 4.5379, 2.9671]) - 0.1
 
-    def go_init_joint(self):
-        # init_gripper_joints = np.array([-0.57789973, -0.70800994,  0.53129942,  1.58402704,  0.31958843, 1.0613998 , -0.79267218])
-        # init_ft_joints = np.array([1.32362411, -1.25974886, -0.91948404,  1.96467796,  0.31982947,1.62020271,  1.17286422])
-
-
-        # init_gripper_joints = np.array([-0.48694, -0.74843, 0.451392, 1.56708, 0.28096, 1.096458, -0.6841])
-        # init_ft_joints = np.array([1.381740, -1.236994, -0.93122, 2.100697, 0.37159, 1.663677, 1.17685])
-
-        init_gripper_joints = np.array([-0.586136, -0.745720, 0.52749, 1.55923, 0.3157, 1.063024, -0.81544])
-
-        target_vel = [0.0]*7 + [0.05]
-        target_acc = [0.0]*7 + [10]
-        qinit = init_gripper_joints.tolist() + [0.06]
-
-        max_vel=[0.1] * 7
-        max_acc=[0.3] * 7
-        print("You must be aware of the movements that will occur during the initialization of the robotic arm")
-        # import pdb;pdb.set_trace()
-        self.update_joint_state(qinit, target_vel, target_acc, max_vel=max_vel, max_acc=max_acc)
-        # import pdb;pdb.set_trace()
-    
-    def set_zero_ft(self):
-        self.robot.SwitchMode(self.mode.NRT_PRIMITIVE_EXECUTION)
-        self.robot.ExecutePrimitive("ZeroFTSensor()")
-        while self.robot.busy():
-            time.sleep(1)
-        self.log.info("Sensor zeroing complete")
-        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
-
-    
-    def update_joint_state(self, target_pos, target_vel=[0.0]*7 + [0.05], target_acc=[0.0]*7 + [0.05], max_vel=[0.2] * 7, max_acc=[0.3] * 7):
-        '''
-        8-dof: target [:7] gripper robot cmd, target[7] gripper cmd
-        7-dof: max_vel, max_acc
-        '''
-        gripper_pos, gripper_vel, gripper_acc, gripper_max_vel, gripper_max_acc = \
-            target_pos[:7], target_vel[:7], target_acc[:7], max_vel[:7], max_acc[:7]
-
-        gripper_width = target_pos[7]
-        gripper_velocity = target_vel[7]
-        gripper_force = target_acc[7]
-
-        gripper_pos = np.clip(np.array(gripper_pos), self.joint_limits_low, self.joint_limits_high).tolist()
-        self.robot.SendJointPosition(gripper_pos, gripper_vel, gripper_acc, gripper_max_vel, gripper_max_acc)
-        # self.gripper.move(gripper_width, gripper_velocity, gripper_force)
-    
-    def update_trajectory_state(self, target_pos_list, target_vel, target_acc, max_vel=[0.2] * 7, max_acc=[0.3] * 7):
-        '''
-        [8-dof]: target [:7] gripper robot cmd, target[7] gripper cmd
-        7-dof: max_vel, max_acc
-        '''
-        for target_pos in target_pos_list:
-            gripper_dis, width_dis = self.get_delta_q(target_pos)
-            self.update_joint_state(target_pos, target_vel, target_acc, max_vel, max_acc)
-            while gripper_dis > 0.02 or width_dis > 0.01:
-                time.sleep(0.01)
-                gripper_dis, width_dis = self.get_delta_q(target_pos)
-            
-    
-    def get_delta_q(self, target_pos):
-        gripper_pos, gripper_width = target_pos[:7], target_pos[7]
-        current_q = self.get_q()
-        gripper_dis = np.max(np.abs(np.array(current_q[:7]) - np.array(gripper_pos)))
-        width_dis = np.abs(current_q[7]*2 - gripper_width)
-        return gripper_dis,width_dis
-
-    
-    def init_gripper(self):
-        set_mode = False
-        if self.robot == self.mode.IDLE:
-            self.robot.setMode(self.mode.NRT_JOINT_POSITION)
-            set_mode = True
-        self.gripper = flexivrdk.Gripper(self.robot)
-        self.gripper_states = flexivrdk.GripperStates()
-        if set_mode:
-            self.robot.setMode(self.mode.IDLE)
-    
-    def _get_gripper_state(self):
-        if self.robot == self.mode.IDLE:
-            self.log.info('Gripper control is not available if the robot is in IDLE mode')
-        else:
-            self.gripper.getGripperStates(self.gripper_states)
-            # print("width: ", round(self.gripper_states.width, 2))
-            # print("force: ", round(self.gripper_states.force, 2))
-            # print("max_width: ", round(self.gripper_states.maxWidth, 2))
-            # print("is_moving: ", self.gripper_states.isMoving)
-        return self.gripper_states
-
-    def get_gripper_width(self):
-        return round(self._get_gripper_state().width,4)
-    
-    def get_q(self):
-        gripper_joint = self.get_joint_pos()
-        gripper_width = self.get_gripper_width()
-        return gripper_joint.tolist() + [gripper_width/2,gripper_width/2]
-    
-    def get_ext_wrench(self,base=True):
-        if base:
-            return np.array(self.robot.states().ext_wrench_in_world)
-        else:
-            return np.array(self.robot.states().ext_wrench_in_tcp)
-    
     def init_robot(self):
         robot_sn = self.robot_sn
         self.robot = flexivrdk.Robot(robot_sn)
@@ -163,7 +60,109 @@ class FlexivRobot:
             time.sleep(1)
 
         self.log.info("Robot is now operational")    
+        
+    def update_joint_state(self, target_pos, target_vel=[0.0]*7 + [0.05], target_acc=[0.0]*7 + [0.05], max_vel=[0.2] * 7, max_acc=[0.3] * 7):
+        '''
+        8-dof: target [:7] gripper robot cmd, target[7] gripper cmd
+        7-dof: max_vel, max_acc
+        '''
+        gripper_pos, gripper_vel, gripper_acc, gripper_max_vel, gripper_max_acc = \
+            target_pos[:7], target_vel[:7], target_acc[:7], max_vel[:7], max_acc[:7]
+
+        gripper_width = target_pos[7]
+        gripper_velocity = target_vel[7]
+        gripper_force = target_acc[7]
+
+        gripper_pos = np.clip(np.array(gripper_pos), self.joint_limits_low, self.joint_limits_high).tolist()
+        self.robot.SendJointPosition(gripper_pos, gripper_vel, gripper_acc, gripper_max_vel, gripper_max_acc)
+        # self.gripper.move(gripper_width, gripper_velocity, gripper_force)
     
+
+    def go_init_joint(self):
+        # init_gripper_joints = np.array([-0.57789973, -0.70800994,  0.53129942,  1.58402704,  0.31958843, 1.0613998 , -0.79267218])
+        # init_ft_joints = np.array([1.32362411, -1.25974886, -0.91948404,  1.96467796,  0.31982947,1.62020271,  1.17286422])
+
+
+        # init_gripper_joints = np.array([-0.48694, -0.74843, 0.451392, 1.56708, 0.28096, 1.096458, -0.6841])
+        # init_ft_joints = np.array([1.381740, -1.236994, -0.93122, 2.100697, 0.37159, 1.663677, 1.17685])
+
+        init_gripper_joints = np.array([-0.586136, -0.745720, 0.52749, 1.55923, 0.3157, 1.063024, -0.81544])
+
+        target_vel = [0.0]*7 + [0.05]
+        target_acc = [0.0]*7 + [10]
+        qinit = init_gripper_joints.tolist() + [0.06]
+
+        max_vel=[0.1] * 7
+        max_acc=[0.3] * 7
+        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
+        # import pdb;pdb.set_trace()
+        self.update_joint_state(qinit, target_vel, target_acc, max_vel=max_vel, max_acc=max_acc)
+        # import pdb;pdb.set_trace()
+    
+    def update_trajectory_state(self, target_pos_list, target_vel, target_acc, max_vel=[0.2] * 7, max_acc=[0.3] * 7):
+        '''
+        [8-dof]: target [:7] gripper robot cmd, target[7] gripper cmd
+        7-dof: max_vel, max_acc
+        '''
+        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
+        for target_pos in target_pos_list:
+            gripper_dis, width_dis = self.get_delta_q(target_pos)
+            self.update_joint_state(target_pos, target_vel, target_acc, max_vel, max_acc)
+            while gripper_dis > 0.02 or width_dis > 0.01:
+                time.sleep(0.01)
+                gripper_dis, width_dis = self.get_delta_q(target_pos)
+            
+    
+    def get_delta_q(self, target_pos):
+        gripper_pos, gripper_width = target_pos[:7], target_pos[7]
+        current_q = self.get_q()
+        gripper_dis = np.max(np.abs(np.array(current_q[:7]) - np.array(gripper_pos)))
+        width_dis = np.abs(current_q[7]*2 - gripper_width)
+        return gripper_dis,width_dis
+
+    def set_zero_ft(self):
+        self.robot.SwitchMode(self.mode.NRT_PRIMITIVE_EXECUTION)
+        self.robot.ExecutePrimitive("ZeroFTSensor()")
+        while self.robot.busy():
+            time.sleep(1)
+        self.log.info("Sensor zeroing complete")
+        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
+
+    def init_gripper(self):
+        set_mode = False
+        if self.robot == self.mode.IDLE:
+            self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
+            set_mode = True
+        self.gripper = flexivrdk.Gripper(self.robot)
+        self.gripper_states = flexivrdk.GripperStates()
+        if set_mode:
+            self.robot.SwitchMode(self.mode.IDLE)
+    
+    def _get_gripper_state(self):
+        if self.robot == self.mode.IDLE:
+            self.log.info('Gripper control is not available if the robot is in IDLE mode')
+        else:
+            self.gripper.getGripperStates(self.gripper_states)
+            # print("width: ", round(self.gripper_states.width, 2))
+            # print("force: ", round(self.gripper_states.force, 2))
+            # print("max_width: ", round(self.gripper_states.maxWidth, 2))
+            # print("is_moving: ", self.gripper_states.isMoving)
+        return self.gripper_states
+
+    def get_gripper_width(self):
+        return round(self._get_gripper_state().width,4)
+    
+    def get_q(self):
+        gripper_joint = self.get_joint_pos()
+        gripper_width = self.get_gripper_width()
+        return gripper_joint.tolist() + [gripper_width/2,gripper_width/2]
+    
+    def get_ext_wrench(self,base=True):
+        if base:
+            return np.array(self.robot.states().ext_wrench_in_world)
+        else:
+            return np.array(self.robot.states().ext_wrench_in_tcp)
+        
     def is_stopped(self):
         """Check if robot is stopped."""
         return self.robot.isStopped()
@@ -185,14 +184,14 @@ class FlexivRobot:
             RuntimeError: error occurred when mode is None.
         """
         if matrix:
-            tcppose = np.array(self._get_robot_status().tcpPose)
+            tcppose = np.array(self.robot().tcpPose)
             pose = np.identity(4)
             # pose[:3,:3] = Rot.from_quat(np.array(tcppose[4:].tolist()+[tcppose[3]])).as_matrix()
             # pose[:3,:3] = Rot.from_quat(tcppose[3:],scalar_first=True).as_matrix()
             pose[:3,:3] = Rot.from_quat(np.array([tcppose[4],tcppose[5],tcppose[6],tcppose[3]])).as_matrix()
             pose[:3,3] = np.array(tcppose[:3])
             return pose
-        return np.array(self._get_robot_status().tcpPose)
+        return np.array(self.robot().tcpPose)
     
     def get_tcp_vel(self):
         """get current robot's tool velocity in world frame.
@@ -203,7 +202,7 @@ class FlexivRobot:
         Raises:
             RuntimeError: error occurred when mode is None.
         """
-        return np.array(self._get_robot_status().tcpVel)
+        return np.array(self.robot().tcpVel)
     
     def get_joint_pos(self):
         """get current joint value.
@@ -214,7 +213,7 @@ class FlexivRobot:
         Raises:
             RuntimeError: error occurred when mode is None.
         """
-        return np.array(self._get_robot_status().q)
+        return np.array(self.robot.states().q)
     
     def get_joint_vel(self):
         """get current joint velocity.
@@ -225,7 +224,7 @@ class FlexivRobot:
         Raises:
             RuntimeError: error occurred when mode is None.
         """
-        return np.array(self._get_robot_status().dq)
+        return np.array(self.robot().dq)
     
 
 if __name__ == "__main__":
