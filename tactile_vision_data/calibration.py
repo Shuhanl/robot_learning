ef236f0298b3660aa19b3f7dcc3779e3578a4419
ShuhanLi <shuhan.li@flexiv.com>
2024-09-24 17:59:22 -0700

    Camera calibration works

--- tactile_vision_data/calibration.py
+++ tactile_vision_data/calibration.py
@@ -1,4 +1,4 @@
-from camera import *
+from camera.camera import *
 import numpy as np
 import cv2
 from robot.flexiv import FlexivRobot
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import time
 
 class Calibration(object):
-    def __init__(self,pattern_size=(8,6),square_size=15,handeye='EIH'):
+    def __init__(self,pattern_size=(9,6),square_size=21,handeye='EIH'):
         '''
 
         :param image_list:  image array, num*720*1280*3
@@ -31,28 +31,37 @@ class Calibration(object):
         self.imgpoints = []  # 2d points in image plane.
         self.criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    def detectFeature(self,color,show=True):
-        img = color
+    def detect_feature(self,img):
+
         self.gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        ret, corners = cv2.findChessboardCorners(img, self.pattern_size, None,
+        ret, corners = cv2.findChessboardCorners(self.gray, self.pattern_size, None,
                                                  cv2.CALIB_CB_ADAPTIVE_THRESH)  # + cv2.CALIB_CB_NORMALIZE_IMAGE+ cv2.CALIB_CB_FAST_CHECK)
         if ret == True:
             self.objpoints.append(self.objp)
             # corners2 = corners
             if (cv2.__version__).split('.')[0] == '2':
-                # pdb.set_trace()
                 cv2.cornerSubPix(self.gray, corners, (5, 5), (-1, -1), self.criteria)
                 corners2 = corners
             else:
                 corners2 = cv2.cornerSubPix(self.gray, corners, (5, 5), (-1, -1), self.criteria)
             self.imgpoints.append(corners2)
-            if show:
-                fig, ax = plt.subplots(figsize=(20, 20))
-                ax.imshow(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-                plt.title('img with feature point')
-                for i in range(self.pattern_size[0] * self.pattern_size[1] - 3):
-                    ax.plot(corners2[i, 0, 0], corners2[i, 0, 1], 'r+')
-                plt.show()
+     
+            fig, ax = plt.subplots(figsize=(20, 20))
+            ax.imshow(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+            plt.title('img with feature point')
+            
+            for i in range(self.pattern_size[0] * self.pattern_size[1] - 3):
+                ax.plot(corners2[i, 0, 0], corners2[i, 0, 1], 'r+')
+            
+            # Generate a unique filename using a timestamp
+            timestamp = time.strftime("%Y%m%d-%H%M%S")
+            filename = f'image_with_feature_points_{timestamp}.png'
+            
+            # Save the figure without showing it
+            plt.savefig(filename, bbox_inches='tight', dpi=300)  # Save with high resolution
+            plt.close()  # Close the figure to free memory
+
+            print(f"Image saved as {filename}")
 
     """ Convert a rotation vector to a 4x4 transformation matrix """
     def rodrigues_trans2tr(self, rvec, tvec):
@@ -75,8 +84,9 @@ class Calibration(object):
         return T
     
     def perform_camera_calibration(self):
+
         if self.imgpoints:
-            gray_shape = cv2.cvtColor(self.imgpoints[0], cv2.COLOR_BGR2GRAY).shape[::-1]
+            gray_shape = self.gray.shape[::-1]
             ret, self.camera_matrix, self.dist_coeffs, self.rvecs, self.tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, gray_shape, None, None)
             return self.camera_matrix, self.dist_coeffs, self.rvecs, self.tvecs
         else:
@@ -89,7 +99,10 @@ class Calibration(object):
         :param robot_poses: Array of robot poses.
         :param camera_transforms: Array of camera transformations corresponding to robot poses.
         '''
+
+        robot_poses = np.array(robot_poses)
         camera_transforms = [self.rodrigues_trans2tr(rvec, tvec / 1000.) for rvec, tvec in zip(rvecs, tvecs)]
+        camera_transforms = np.array(camera_transforms)
         rot, pos = cv2.calibrateHandEye(robot_poses[:, :3, :3], robot_poses[:, :3, 3], camera_transforms[:, :3, :3], camera_transforms[:, :3, 3])
         camT = np.identity(4)
         camT[:3, :3] = rot
@@ -99,21 +112,23 @@ class Calibration(object):
 
 if __name__ == "__main__":
     cam = RealSenseCamera()
-    calib = Calibration(pattern_size=(8,6), square_size=15)
+    calib = Calibration(pattern_size=(9,6), square_size=21)
     flexiv_robot = FlexivRobot()
 
-    cartesian_list = [[0.41219, -0.807, -0.922, -1.858, 0.168, 1.870, 0.0011],
-                [-0.314, -1.01202, 0.180, -2.315, 0.401, 2.122, 0.589],
-                [-1.364, -1.540, 0.583, -1.33, 1.012, 1.35, 1.38]]
+    cartesian_list = [[0.69569635, -0.10836965,  0.14336556,  0.01396798, -0.00246993,  0.99989706,  0.00214556],
+                      [0.59569635, -0.10836965,  0.14336556,  0.01396798, -0.00246993,  0.99989706,  0.00214556],
+                      [0.69569635, -0.00836965,  0.14336556,  0.01396798, -0.00246993,  0.99989706,  0.00214556]]
 
+    robot_poses = []
     # Assuming cam and panda are predefined objects
     for cartesian in cartesian_list:
         flexiv_robot.cartesian_motion_force_control(cartesian)
+        robot_poses.append(flexiv_robot.get_tcp_pose(matrix = True))
         color, depth = cam.get_data()
-        cam.detect_feature(color)
+        calib.detect_feature(color)
+        time.sleep(1)
 
     mtx, dist, rvecs, tvecs = calib.perform_camera_calibration()
     mtx = cam.getIntrinsics()
-    camT = calib.perform_hand_eye_calibration(cartesian_list, rvecs, tvecs)
-    np.save('../config/camera_calib.npy', camT)
-    time.sleep(1)
+    camT = calib.perform_hand_eye_calibration(robot_poses, rvecs, tvecs)
+    np.save('config/camera_calib.npy', camT)
--- tactile_vision_data/robot/flexiv.py
+++ tactile_vision_data/robot/flexiv.py
@@ -30,7 +30,7 @@ class FlexivRobot:
         self.gripper = None
 
         self.init_robot()
-        self.init_gripper()
+        # self.init_gripper()
 
         self.joint_limits_low = np.array([-2.7925, -2.2689, -2.9671, -1.8675, -2.9671, -1.3963, -2.9671]) + 0.1
         self.joint_limits_high = np.array([2.7925, 2.2689, 2.9671, 2.6878, 2.9671, 4.5379, 2.9671]) - 0.1
@@ -63,14 +63,13 @@ class FlexivRobot:
         self.log.info("Robot is now operational")    
 
     def init_gripper(self):
-        set_mode = False
-        if self.robot == self.mode.IDLE:
-            self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
-            set_mode = True
+
+        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
+
+        self.log.info("Initializing gripper, this process takes about 10 seconds ...")
         self.gripper = flexivrdk.Gripper(self.robot)
-        self.gripper_states = flexivrdk.GripperStates()
-        if set_mode:
-            self.robot.SwitchMode(self.mode.IDLE)
+        self.gripper.Init()
+        self.log.info("Initialization complete")
 
     def move_to_home(self):
         # Move robot to home pose
@@ -78,54 +77,29 @@ class FlexivRobot:
         self.robot.SwitchMode(self.mode.NRT_PRIMITIVE_EXECUTION)
         self.robot.ExecutePrimitive("Home()")
 
-    def update_joint_state(self, target_pos, target_vel=[0.0]*7, target_acc=[0.0]*7, max_vel=[0.2] * 7, max_acc=[0.3] * 7):
+        # Wait for the primitive to finish
+        while self.robot.busy():
+            time.sleep(1)
+        
+    def joint_control(self, target_pos, target_vel, target_acc, max_vel=[0.2] * 7, max_acc=[0.3] * 7):
         '''
-        7-dof: target [:7] robot cmd
+        [7-dof]: target [:7] gripper robot cmd
         7-dof: max_vel, max_acc
         '''
-        gripper_pos, gripper_vel, gripper_acc, gripper_max_vel, gripper_max_acc = \
+        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
+        joint_pos, joint_vel, joint_acc, joint_max_vel, joint_max_acc = \
             target_pos[:7], target_vel[:7], target_acc[:7], max_vel[:7], max_acc[:7]
 
-        gripper_pos = np.clip(np.array(gripper_pos), self.joint_limits_low, self.joint_limits_high).tolist()
-        self.robot.SendJointPosition(gripper_pos, gripper_vel, gripper_acc, gripper_max_vel, gripper_max_acc)
-    
-    def update_gripper_states(self, gripper_width, gripper_velocity, gripper_force):
-        
-        self.gripper.Move(gripper_width, gripper_velocity, gripper_force)
-    
-
-    def go_init_joint(self):
-
-        init_gripper_joints = np.array([-0.586136, -0.745720, 0.52749, 1.55923, 0.3157, 1.063024, -0.81544])
-
-        target_vel = [0.0]*7 + [0.05]
-        target_acc = [0.0]*7 + [10]
-        qinit = init_gripper_joints.tolist() + [0.06]
-
-        max_vel=[0.1] * 7
-        max_acc=[0.3] * 7
-        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
-        # import pdb;pdb.set_trace()
-        self.update_joint_state(qinit, target_vel, target_acc, max_vel=max_vel, max_acc=max_acc)
-        # import pdb;pdb.set_trace()
-    
-    def joint_control(self, target_pos_list, target_vel, target_acc, max_vel=[0.2] * 7, max_acc=[0.3] * 7):
-        '''
-        [8-dof]: target [:7] gripper robot cmd, target[7] gripper cmd
-        7-dof: max_vel, max_acc
-        '''
-        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
-        for target_pos in target_pos_list:
-            gripper_dis, width_dis = self.get_delta_q(target_pos)
-            self.update_joint_state(target_pos, target_vel, target_acc, max_vel, max_acc)
-            while gripper_dis > 0.02 or width_dis > 0.01:
-                time.sleep(0.01)
-                gripper_dis, width_dis = self.get_delta_q(target_pos)
+        joint_pos = np.clip(np.array(joint_pos), self.joint_limits_low, self.joint_limits_high).tolist()
+        self.robot.SendJointPosition(joint_pos, joint_vel, joint_acc, joint_max_vel, joint_max_acc)
             
     def cartesian_motion_force_control(self, target_pose):
             
         self.robot.SwitchMode(self.mode.NRT_CARTESIAN_MOTION_FORCE)
         self.robot.SendCartesianMotionForce(target_pose)
+
+    def gripper_control(self, gripper_width, gripper_velocity, gripper_force):
+        self.gripper.Move(gripper_width, gripper_velocity, gripper_force)
 
     def get_delta_q(self, target_pos):
         gripper_pos, gripper_width = target_pos[:7], target_pos[7]
@@ -137,43 +111,25 @@ class FlexivRobot:
     def set_zero_ft(self):
         self.robot.SwitchMode(self.mode.NRT_PRIMITIVE_EXECUTION)
         self.robot.ExecutePrimitive("ZeroFTSensor()")
+
         while self.robot.busy():
             time.sleep(1)
         self.log.info("Sensor zeroing complete")
-        self.robot.SwitchMode(self.mode.NRT_JOINT_POSITION)
-    
-    def _get_gripper_state(self):
-        if self.robot == self.mode.IDLE:
-            self.log.info('Gripper control is not available if the robot is in IDLE mode')
-        else:
-            self.gripper.getGripperStates(self.gripper_states)
-            print("width: ", round(self.gripper_states.width, 2))
-            print("force: ", round(self.gripper_states.force, 2))
-            print("max_width: ", round(self.gripper_states.maxWidth, 2))
-            print("is_moving: ", self.gripper_states.isMoving)
-        return self.gripper_states
 
-    def get_gripper_width(self):
-        return round(self._get_gripper_state().width,4)
     
-    def get_q(self):
-        gripper_joint = self.get_joint_pos()
-        gripper_width = self.get_gripper_width()
-        return gripper_joint.tolist() + [gripper_width/2,gripper_width/2]
+    def get_gripper_states(self):
+
+        return self.gripper.states().width, self.gripper.states().force
+
     
     def get_ext_wrench(self,base=True):
         if base:
             return np.array(self.robot.states().ext_wrench_in_world)
         else:
             return np.array(self.robot.states().ext_wrench_in_tcp)
-        
-    def is_stopped(self):
-        """Check if robot is stopped."""
-        return self.robot.isStopped()
-    
+            
     def is_connected(self):
         """return if connected.
-
         Returns: True/False
         """
         return self.robot.connected()    
@@ -188,14 +144,13 @@ class FlexivRobot:
             RuntimeError: error occurred when mode is None.
         """
         if matrix:
-            tcppose = np.array(self.robot().tcpPose)
+            tcppose = np.array(self.robot.states().tcp_pose)
             pose = np.identity(4)
-            # pose[:3,:3] = Rot.from_quat(np.array(tcppose[4:].tolist()+[tcppose[3]])).as_matrix()
-            # pose[:3,:3] = Rot.from_quat(tcppose[3:],scalar_first=True).as_matrix()
             pose[:3,:3] = Rot.from_quat(np.array([tcppose[4],tcppose[5],tcppose[6],tcppose[3]])).as_matrix()
             pose[:3,3] = np.array(tcppose[:3])
             return pose
-        return np.array(self.robot().tcpPose)
+        
+        return np.array(self.robot.states().tcp_pose)
     
     def get_tcp_vel(self):
         """get current robot's tool velocity in world frame.
@@ -206,7 +161,7 @@ class FlexivRobot:
         Raises:
             RuntimeError: error occurred when mode is None.
         """
-        return np.array(self.robot.states().tcpVel)
+        return np.array(self.robot.states().tcp_vel)
     
     def get_joint_pos(self):
         """get current joint value.
@@ -228,25 +183,43 @@ class FlexivRobot:
         Raises:
             RuntimeError: error occurred when mode is None.
         """
-        return np.array(self.robot().dq)
+        return np.array(self.robot.states().dq)
     
 
 if __name__ == "__main__":
     flexiv_robot = FlexivRobot()
-    
-    init_gripper_joints = np.array([-0.57789973, -0.70800994,  0.53129942,  1.58402704,  0.31958843, 1.0613998 , -0.79267218])
-    
-    target_vel = [0.0]*7 + [0.05] 
-    target_acc = [0.0]*7 + [10]
 
-    qinit = init_gripper_joints.tolist() + [0.5]
+    flexiv_robot.move_to_home()
+    flexiv_robot.set_zero_ft()
+    ext_wrench = flexiv_robot.get_ext_wrench()
+    print("External wrench:", ext_wrench)
+    tcp_pose = flexiv_robot.get_tcp_pose()
+    print("TCP pose:", tcp_pose)
+    gripper_states = flexiv_robot.get_gripper_states()
+    print("Gripper states:", gripper_states)
 
+    cartesian_list = [[0.69569635, -0.10836965,  0.14336556,  0.01396798, -0.00246993,  0.99989706,  0.00214556],
+                      [0.59569635, -0.10836965,  0.14336556,  0.01396798, -0.00246993,  0.99989706,  0.00214556],
+                      [0.69569635, -0.00836965,  0.14336556,  0.01396798, -0.00246993,  0.99989706,  0.00214556]]
+    
+    for cartesian in cartesian_list:
+        flexiv_robot.cartesian_motion_force_control(cartesian)
+        time.sleep(1)
+
+    joints_list = [[-0.57789973, -0.70800994,  0.53129942,  1.58402704,  0.31958843, 1.0613998 , -0.79267218], 
+                   [-0.67789973, -0.80800994,  0.53129942,  1.58402704,  0.31958843, 1.0613998 , -0.79267218]]
+    
+    target_vel = [0.0]*7  
+    target_acc = [0.0]*7 
     max_vel=[0.2] * 7
     max_acc=[0.3] * 7
 
-    flexiv_robot.move_to_home()
-    flexiv_robot.go_init_joint()
-    flexiv_robot.set_zero_ft()
-    flexiv_robot.get_ext_wrench()
+    for joints in joints_list:
+        flexiv_robot.joint_control(joints, target_vel, target_acc, max_vel=max_vel, max_acc=max_acc)
+        time.sleep(1)
+    
+    # gripper_list = [[0.01, 0.1, 20], [0.09, 0.1, 20], [0.01, 0.1, 20]]
+    # for gripper in gripper_list:
+    #     flexiv_robot.gripper_control(gripper[0], gripper[1], gripper[2])
+    #     time.sleep(1)
 
-    flexiv_robot.update_joint_state(qinit, target_vel, target_acc, max_vel=max_vel, max_acc=max_acc)
